5d8a9e6ef30d51c680ac8a24fb3761f55a497e16
Robert Hughes <roberts.ginger.email@gmail.com>
2018-01-28 08:10:17 +0000

    automatically backed up by learn

---
+++ lib/doctor.rb
@@ -0,0 +1,11 @@
+class Doctor
+  attr_reader :name, :appointments
+  def initialize(name)
+    @name = name
+    @appointments = []
+  end
+  def add_appointment(appointment)
+    @appointments << appointment
+    appointment.doctor = self
+  end
+end
